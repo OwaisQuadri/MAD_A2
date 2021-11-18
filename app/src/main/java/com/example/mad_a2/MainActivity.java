2a1545e4ba4a6d848dd8af2c3baee8e06b8ca19e
Owais Quadri <owais.quadri@ontariotechu.net>
2021-11-17 23:53:04 -0500

    geocode fix

--- app/src/main/java/com/example/mad_a2/MainActivity.java
+++ app/src/main/java/com/example/mad_a2/MainActivity.java
@@ -9,10 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.Context;
 import android.widget.TextView;
 
-import java.io.IOException;
 import java.util.Locale;
 import java.util.*;
 
@@ -30,22 +28,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 double longitude, latitude;
                 EditText lng = findViewById(R.id.longEditText);
-                EditText lat = findViewById(R.id.latEditText);
+                EditText lat = findViewById(R.id.lattEditText);
                 TextView address = findViewById(R.id.addressTextView);
                 longitude = Double.parseDouble(lng.getText().toString());
                 latitude = Double.parseDouble(lat.getText().toString());
-                Geocoder g = new Geocoder(getBaseContext());
-                List coords = null;
-                try {
-                    coords = g.getFromLocation(latitude, longitude, 1);
-                } catch (IOException e) {
-                    address.setText("Address: This location does not have an address, try again.");
-                }
-                if (coords != null && coords.size() > 0) {
-                    Address adr = (Address) coords.get(0);
-                    address.setText("Address: " + adr.getAddressLine(0));
+                Geocoder g = new Geocoder((getApplicationContext()),Locale.getDefault());
 
+                List<Address> coords;
+                try {
+                    coords = g.getFromLocation(latitude,longitude , 1);
+                    if (coords != null && coords.size() > 0) {
+                        Address adr = (Address) coords.get(0);
+                        address.setText("Address: " + adr.getAddressLine(0));
+
+                    }
+                } catch (Exception e) {
+                    address.setText("Address: This location does not have an address, try again.");
+                    e.printStackTrace();
                 }
+
 
             }
         });
